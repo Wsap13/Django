c5e7a009f00cfdf32d808fb533db3f546ca65984
Wsap13 <73704923+Wsap13@users.noreply.github.com>
2022-11-05 12:37:50 +0100

    Add
    Dodanie dynamicznych urls oraz strony głównej

--- PierwszeKroki/miesieczne_wyzwania/wyzwania/urls.py
+++ PierwszeKroki/miesieczne_wyzwania/wyzwania/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.str_glowna), #/wyzwania/ (taka trochę strona główna)
     path("<int:miesiac>", views.miesieczne_wyzwania_przez_liczbe),
-    path("<str:miesiac>", views.miesieczne_wyzwania)
+    path("<str:miesiac>", views.miesieczne_wyzwania, name="miesieczne_wyzwanie")
     
 ]
+ 
--- PierwszeKroki/miesieczne_wyzwania/wyzwania/views.py
+++ PierwszeKroki/miesieczne_wyzwania/wyzwania/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 miesiace = {
     "styczeń": "Get lost!",
@@ -18,18 +19,30 @@ miesiace = {
 
 # Create your views here.
 
+def str_glowna(request):
+    list_items = ""
+    miesiac = list(miesiace.keys())
+    for month in miesiac:
+        capitalized_month = month.capitalize()
+        month_path = reverse("miesieczne_wyzwanie", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
+    
+    return HttpResponse(f"<ul>{list_items}</ul>")
+
 
 def miesieczne_wyzwania_przez_liczbe(request, miesiac):
     obecny_miesiac = list(miesiace.keys())
     if miesiac > len(miesiace):
         return HttpResponseNotFound("Something goes wrong")
     poprawny_miesiac = obecny_miesiac[miesiac - 1]
+    redirected_path = reverse("miesieczne_wyzwanie", args=[poprawny_miesiac]) # / wyzwani/miesiac
     return HttpResponseRedirect("/wyzwania/" + poprawny_miesiac)
-
+ 
 
 def miesieczne_wyzwania(request, miesiac):
     try:
         wyzwanie_text = miesiace[miesiac]
-        return HttpResponse(wyzwanie_text)
+        response_data =f"<h1>{wyzwanie_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("You fucked up")
+        return HttpResponseNotFound("<h1>You fucked up!</h1>")
