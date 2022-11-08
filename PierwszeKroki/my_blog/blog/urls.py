44791ee360efb88b933fb59e6cb7b52c51121251
Wsap13 <73704923+Wsap13@users.noreply.github.com>
2022-11-08 08:58:06 +0100

    posty
    dodanie urls do postów

--- PierwszeKroki/my_blog/blog/urls.py
+++ PierwszeKroki/my_blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpattern = [
-     path(""),
-     path("posts"),
-     path("posts/<>")
+     path("", views.strona_startowa),
+     path("posts", views.posty),
+     path("posts/<slug:slug>",views.post_szegol) #/posty/moj-pierwszy-post
  ]
  
--- PierwszeKroki/my_blog/blog/views.py
+++ PierwszeKroki/my_blog/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def strona_startowa(request):
+    pass
+
+def posty(request):
+    pass
+
+def post_szegol(request):
+    pass
