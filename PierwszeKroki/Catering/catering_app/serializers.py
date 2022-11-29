82ade081ffc155a54c8cb0260e6563acef2d72db
Wsap13 <73704923+Wsap13@users.noreply.github.com>
2022-11-29 21:37:58 +0100

    poprawki

--- PierwszeKroki/Catering/catering_app/serializers.py
+++ PierwszeKroki/Catering/catering_app/serializers.py
@@ -11,28 +11,3 @@ class PracownicySerializer(serializers.Serializer):
             raise serializers.ValidationError('Pensja nie może być mniejsza lub równa zero.')
         else:
             return value
-
-    def validate_dane(self, value):
-        if value[0] in string.ascii_lowercase:
-            raise serializers.ValidationError(
-                "Dane powinny zaczynać się z dużej litery"
-            )
-        else:
-            return value
-
-    def validate_harmonogram_pracy(self, value):
-        if 8 > value.any() > 20:
-            raise serializers.ValidationError(
-                "Godziny pracy powinny znajdować się w przedziale 8-20"
-            )
-        else:
-            return value
-
-
-    def validate_harmonogram_spacerow(self, value):
-        if 8 > value.any() > 20:
-            raise serializers.ValidationError(
-                "Godziny spacerów powinny znajdować się w przedziale 8-20"
-            )
-        else:
-            return value
